8a250542f483de71f707e0c86f92fc0f460ae869
Krishnamuti <61639711+Krishnamuti@users.noreply.github.com>
2021-08-07 14:52:14 -0300

    ajustes config webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const path = require('path')
 
 module.exports = {
     entry: './src/index.jsx',
@@ -9,7 +8,7 @@ module.exports = {
         filename: './app.js'
     },
     devServer: {
-        port: 8080,
+        port: 9090,
         contentBase: './public',
     },
     resolve: {
@@ -18,10 +17,10 @@ module.exports = {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
-    module:{
+    module: {
         loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
@@ -30,13 +29,12 @@ module.exports = {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        },{
+        }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
-
 }
