5728110ff4f0547d18a0917d3b0416ae8c21cd47
Krishnamuti <61639711+Krishnamuti@users.noreply.github.com>
2021-08-09 23:03:21 -0300

    Componente PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props =>(
     <div>
         <h1>
-            Sobre
+            <PageHeader name='Sobre' small='Nós'></PageHeader>
+            <h2>Nossa História</h2>
+            <p>O cuidado em identificar pontos críticos no desenvolvimento contínuo de distintas formas de atuação apresenta tendências no sentido de aprovar a manutenção do investimento em reciclagem técnica.</p>
+            <h2>Missão e Visão</h2>
+            <p>O incentivo ao avanço tecnológico, assim como a hegemonia do ambiente político assume importantes posições no estabelecimento dos modos de operação convencionais.</p>
+            <h2>Imprensa</h2>
+            <p>Pensando mais a longo prazo, o acompanhamento das preferências de consumo causa impacto indireto na reavaliação das diversas correntes de pensamento.</p>
         </h1>
     </div>
 )
