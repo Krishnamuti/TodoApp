f03345bf27b777e3c35321adccbac56741f57e6a
Krishnamuti <61639711+Krishnamuti@users.noreply.github.com>
2022-06-28 21:48:19 -0300

    middleware (Redux-Promise)

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,18 +1,6 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar cartão de crédito',
-        done: true
-    },{
-        _id: 2,
-        description: 'Reunião com equipe',
-        done: false
-    },{
-        _id: 3,
-        description: 'Levar filho na consulta médica',
-        done: false
-    }]
+    description: '', 
+    list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
